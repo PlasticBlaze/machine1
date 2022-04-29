f10200e23314e559d02cff50ce375bf953b623ad
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2022-04-28 20:43:18 -0400

    Create extrastepper.py

---
+++ extrastepper.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+import time
+
+################################
+# RPi and Motor Pre-allocations
+################################
+#
+#define GPIO pins
+direction= 22 # Direction (DIR) GPIO Pin
+step = 23 # Step GPIO Pin
+EN_pin = 24 # enable pin (LOW to enable)
+
+# Declare a instance of class pass GPIO pins numbers and the motor type
+mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
+GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
+
+###########################
+# Actual motor control
+###########################
+#
+GPIO.output(EN_pin,GPIO.LOW) # pull enable to low to enable motor
+mymotortest.motor_go(True, # True=Clockwise, False=Counter-Clockwise
+                     "1/8" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                     15000, # number of steps
+                     .000075, # step delay [sec]
+                     False, # True = print verbose output
+                     .05) # initial delay [sec]
+
+#GPIO.output(EN_pin,GPIO.LOW) # pull enable to low to enable motor
+#mymotortest.motor_go(False, # True=Clockwise, False=Counter-Clockwise
+                     #"1/8" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                     #1500, # number of steps
+                     #.000075, # step delay [sec]
+                     #False, # True = print verbose output
+                     #.05) # initial delay [sec]
+GPIO.cleanup() # clear GPIO allocations after run
