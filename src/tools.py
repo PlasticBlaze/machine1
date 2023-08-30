6fc565f34c6cbec08ca6a5d28bcb072ee7ad0e64
Fab <fpuentem@uni.pe>
2023-08-30 19:48:55 -0300

    Source code to src folder

---
+++ src/tools.py
@@ -0,0 +1,17 @@
+"""
+Functions used to adjust parameters
+"""
+
+
+def tool_ofset():
+    """
+    Further description in the future
+    """
+    print("tool_ofset processing")
+
+
+def fix_divot():
+    """
+    Further description in the future
+    """
+    print("fix_divot processing")
