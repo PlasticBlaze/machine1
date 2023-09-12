3031cac05fe6f248a4566725349637ebeaf9639a
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2023-09-12 00:44:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -328,7 +328,7 @@ if __name__ == "__main__":
         point = getpoint()
 
         # check function returning True if the robot is still within the boundaries
-        if check(point1):
+        if check(point):
             # routine function returning True if the imager is centered to the found contour
             if routine():
                 # End loop when the robot is centered
