98fec02d3e2e577d2ceeb2e28c733ad0e9b6b460
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2023-09-14 21:46:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -329,7 +329,7 @@ if __name__ == "__main__":
         point = getpoint()
 
         # check function returning True if the robot is still within the boundaries
-        if check(point1, PolygonHole):
+        if check(point, PolygonHole):
             # routine function returning True if the imager is centered to the found contour
             if routine():
                 # End loop when the robot is centered
