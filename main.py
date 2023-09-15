6f68daa3839696339d51b04ccd528fd6484771be
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2023-09-14 22:45:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -308,7 +308,7 @@ def check(PolygonHole, Location):
 
     # Return a boolean
     if pos_check:
-        return True, Location
+        return True
     else:
         return False
 
@@ -330,7 +330,7 @@ if __name__ == "__main__":
         now = getpoint()
 
         # check function returning True if the robot is still within the boundaries
-        if check(now, Location):
+        if check(now):
             # routine function returning True if the imager is centered to the found contour
             if routine():
                 # End loop when the robot is centered
