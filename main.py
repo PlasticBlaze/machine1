08a16415e94364f310c1b834da85f8bc3fab9082
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2023-09-16 21:52:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def routine():
 
     json_file = json.load(
         open(
-            f"{PROJECT_PATH}/src/config/json_string_testing.json",
+            f"{PROJECT_PATH}/src/config/json_string-testing.json",
             encoding="utf-8",
         )
     )
