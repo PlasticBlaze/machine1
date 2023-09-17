bd4dc71d656caffe63adec52b60b5616d580d430
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2023-09-16 22:42:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -264,8 +264,8 @@ def getpoint():
     Location = Point(-85.5911993, 38.2348362)
     return Location
 
-#def check(Location, polygon):
-def check(Location):
+def check(Location, polygon):
+
     """
     Function to check if the robot is still within the boundaries,
     if false, turn around and get back in
