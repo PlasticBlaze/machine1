2270db9359a9079415925a48da6222353c1c43fa
PlasticBlaze <77796405+PlasticBlaze@users.noreply.github.com>
2023-09-18 21:42:40 -0400

    Update tools.py

--- src/tools.py
+++ src/tools.py
@@ -1,7 +1,8 @@
 """
 Functions used to adjust parameters
 """
-
+import serial
+import time 
 
 def tool_offset():
     """
@@ -12,6 +13,86 @@ def tool_offset():
 
 def fix_divot():
     """
-    Further description in the future
+    Controls servo.
     """
+    import serial
+    import time
+    
+    # configure the serial connections (the parameters differs on the device you are connecting to)
+    serial1 = serial.Serial('COM5', 115200)
+    
+    # SEND 'ALARM RESET'
+    serial1.write(b':01050407FF00F0\r\n')
+    time.sleep(.2)
+    
+    # SEND 'PIO SWITCHING OFF'
+    serial1.write(b':010504270000CF\r\n')
+    time.sleep(.2)
+    
+    # SEND 'PIO SWITCHING ON'
+    serial1.write(b':01050427FF00D0\r\n')
+    time.sleep(.2)
+    
+    # SEND 'SERVO OFF'
+    serial1.write(b':010504030000F3\r\n')
+    time.sleep(.2)
+    
+    # SEND 'SERVO ON'
+    serial1.write(b':01050403FF00F4\r\n')
+    time.sleep(.2)
+    
+    # SEND 'ALARM RESET'
+    serial1.write(b':01050407FF00F0\r\n')
+    time.sleep(.2)
+    
+    
+    # Divot Loop
+    
+    # SEND 'POSITION 0'
+    serial1.write(b':01069800000061\r\n')
+    time.sleep(.5)
+    
+    # SEND 'POSITION 1'
+    serial1.write(b':01069800000160\r\n')
+    time.sleep(1)
+    
+    # SEND 'POSITION 2'
+    serial1.write(b':0106980000025F\r\n')
+    time.sleep(.5)
+    
+    # SEND 'POSITION 3'
+    serial1.write(b':0106980000035E\r\n')
+    time.sleep(.5)
+    
+    # REPEATED
+    
+    # SEND 'POSITION 2'
+    serial1.write(b':0106980000025F\r\n')
+    time.sleep(.5)
+    
+    # SEND 'POSITION 3'
+    serial1.write(b':0106980000035E\r\n')
+    time.sleep(.5)
+    
+    # SEND 'POSITION 4'
+    #serial1.write(b':0106980000045D\r\n')
+    #time.sleep(.5)
+    
+    
+    # SEND 'POSITION 0'
+    serial1.write(b':01069800000061\r\n')
+    time.sleep(.5)
+    
+    # SEND 'POSITION 5'
+    #serial1.write(b':0106980000055C\r\n')
+    #time.sleep(.5)
+    
+    # SEND 'ALARM RESET'
+    serial1.write(b':01050407FF00F0\r\n')
+    time.sleep(.5)
+    
+    # SEND 'SERVO OFF'
+    serial1.write(b':010504030000F3\r\n')
+    time.sleep(.5)
+    
     print("fix_divot processing")
